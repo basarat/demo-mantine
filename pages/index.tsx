57065c1c8afff04513db8d020b23268383c15b35
basarat <basaratali@gmail.com>
2022-05-30 09:05:14 +1000

    wrap

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import type { NextPage } from 'next';
+import { TextInput, Group } from '@mantine/core';
 
 const Home: NextPage = () => {
   return (
-    null
+    <Group direction="column" spacing="xl">
+      <TextInput label="What are you doing?" placeholder="Demo" />
+      <TextInput label="How often do you do that?" placeholder="Every Week" />
+    </Group>
   );
 }
 
